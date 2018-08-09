46e750382b3f1dc3a837bb3a31ac9d5a7f0ce614
nishantpoorswani <40753017+nishantpoorswani@users.noreply.github.com>
2018-08-09 12:15:14 +0530

    Add files via upload

--- pc/mqtt_publish_demo.py
+++ pc/mqtt_publish_demo.py
@@ -1,1 +1,10 @@
+# MQTT Publish Demo
+# Publish two messages, to two different topics
 
+import paho.mqtt.publish as publish
+
+print("please enter msg:")
+msg = raw_input()
+publish.single("nishant/test", msg, hostname="test.mosquitto.org")
+print("Done")
+ 
